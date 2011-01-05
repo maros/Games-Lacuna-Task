f790bd8668d5bbb4d6765d1a2d015e980a675f3e
Maros Kollar <maros@k-1.com>
2011-01-05 17:42:35 +0100

    Simple upgrade task

--- lib/Games/Lacuna/Task/Action/Upgrade.pm
+++ lib/Games/Lacuna/Task/Action/Upgrade.pm
@@ -1,122 +1,107 @@
-package Games::Lacuna::Task::Upgrade;
-use Moose;
+package Games::Lacuna::Task::Action::Upgrade;
+
 use 5.010;
-with qw(
-    Games::Lacuna::Task::Role::Connect
+
+use Moose;
+use List::Util qw(max);
+
+with qw(Games::Lacuna::Task::Role::Client
+    Games::Lacuna::Task::Role::Helper
     Games::Lacuna::Task::Role::Logger);
 
-our %BUILDINGS = (
-    'Grove of Trees'            => 'ignore',
-    'Rocky Outcropping'         => 'ignore',
-    'Lake'                      => 'ignore',
-    'Patch of Sand'             => 'ignore',
-    'Crater'                    => 'ignore',
-    
-    'Waste Recycling Center'    => 'ignore',
-    'Development Ministry'      => 'ignore',
-    'Network 19 Affiliate'      => 'ignore',
-    'University'                => 'ignore',
-    'Observatory'               => 'ignore',
-    'Shipyard'                  => 'ignore',
-    'Space Port'                => 'ignore',
-    
-    'Energy Reserve'            => 6,
-    'Food Reserve'              => 6,
-    'Water Storage Tank'        => 6,
+has 'start_building_at' => (
+    isa     => 'Int',
+    is      => 'rw',
+    required=> 1,
+    default => 1,
+);
 
-    'Algae Syrup Bottler'       => 5,
-    'Malcud Burger Packer'      => 5,
-
-    'Malcud Fungus Farm'        => 4,
-    'Mine'                      => 4,
-
-    'Algae Cropper'             => 3,
-    'Geo Energy Plant'          => 3,
-    'Water Purification Plant'  => 3,
-
-    'Ore Storage Tanks'         => 2,
-    'Waste Recycling Center'    => 2,
-    'Planetary Command Center'  => 2,
-
-    'Waste Sequestration Well'  => 1,
+has 'upgrade_preference' => (
+    isa     => 'ArrayRef[Str]',
+    is      => 'rw',
+    default => sub {
+        [
+            'Waste Sequestration Well',
+            'Ore Storage Tanks',
+            'Water Storage Tank',
+            'Food Reserve',
+            'Energy Reserve',
+            'Oversight Ministry',
+            'Security Ministry',
+        ]
+    },
 );
 
 sub run {
     my ($self) = @_;
     
-    my $planets = $self->session->planets();
+    my $university_level = $self->university_level;
     
     # Loop all planets
-    foreach my $planet (@$planets) {
-        my $max_level = 1;
-        my %upgrade_queue;
+    PLANETS:
+    foreach my $planet_stats ($self->planets) {
+        $self->log('info',"Processing planet %s",$planet_stats->{name});
         
-        # Get max level
-        my $university = $planet->get_university();
-        $max_level += $university->level
-            if defined $university;
+        my $building_count = 0;
+        my @levels;
         
-        # Get all buildings
-        my $buildings = $planet->get_buildings();
-        foreach my $building (@$buildings) {
-            # Check if building is at max level
-            next
-                if $building->level >= $max_level;
-            # Check if construction is under way
-            next
-                if $building->build_remaining > 0;
-            # Check if we know this building type
-            unless (defined $BUILDINGS{$building->name}) {
-                say('Unknown building type '.$building->name);
-                next;
-            }
-            # Get building priority
-            my $priority = $BUILDINGS{$building->name};
-            # Check if building should be ignored
-            next
-                if $priority eq 'ignore';
-            # Add to upgrade queue
-            $upgrade_queue{$priority} ||= [];
-            push(@{$upgrade_queue{$priority}},$building); 
+        my $buildings = $self->buildings_body($planet_stats->{id});
+        
+        # Get build queue size
+        foreach my $building_id (keys %$buildings) {
+            $building_count ++
+                if defined $buildings->{$building_id}{pending_build};
+            push(@levels,$buildings->{$building_id}{level});
         }
         
-        # Process upgrade queue by priority
-        #sort values %BUILDINGS
-        foreach my $priority (1..10) {
-            my @reasons;
-            
-            # Check queue size
-            next
-                unless defined $upgrade_queue{$priority}
-                && scalar(@{$upgrade_queue{$priority}});
-            
-            # Loop all buildings in queue
-            foreach my $building (sort { $a->level <=> $b->level } 
-                @{$upgrade_queue{$priority}}) {
-                # Get building data
-                my $building_data = $building->view;
-                # Check if building is upgradeable
-                unless ($building_data->{building}{upgrade}{can}) {
-                    unshift(@{$building_data->{building}{upgrade}{reason}},$building->name);
-                    push(@reasons,$building_data->{building}{upgrade}{reason});
-                    next;
+        my $max_level = max(@levels);
+        
+        # Check if build queue is filled
+        if ($building_count <= $self->start_building_at) {
+            # Get first upgradeable building
+            for my $check (1,0) {
+                foreach my $building_type (@{$self->{upgrade_preference}}) {
+                    foreach my $building_id (keys %$buildings) {
+                        my $building_data = $buildings->{$building_id};
+                        next
+                            unless $building_data->{name} eq $building_type;
+                        next
+                            if $building_data->{pending_build};
+                        next
+                            if $building_data->{level} > $university_level;
+                        next
+                            if $building_data->{level} >= $max_level && $check;
+                        
+                        my $building_class = $self->building_class($building_data->{url});
+                        
+                        my $building_object = $building_class->new(
+                            client      => $self->client->client,
+                            id          => $building_id,
+                        );
+                        
+                        my $building_detail = $self->request(
+                            object  => $building_object,
+                            method  => 'view',
+                        );
+                        
+                        next
+                            unless $building_detail->{building}{upgrade}{can};
+                        
+                        $self->request(
+                            object  => $building_object,
+                            method  => 'upgrade',
+                        );
+                        
+                        $self->log('notice',"Upgrading %s on %s",$building_type,$planet_stats->{name});
+                        
+                        $self->clear_cache('body/'.$planet_stats->{id}.'/buildings');
+                        
+                        next PLANETS;
+                    }
                 }
-                # Upgrade building
-                $building->upgrade();
-                say('Upgrading '.$building->name);
-                next;
             }
-            # Print why we could not upgrade anything
-            if (scalar @reasons) {
-                say('Cannot upgrade:');
-                foreach my $reason (@reasons) {
-                    say("* ".$reason->[0].': '.$reason->[2].' ('.$reason->[3].')');
-                }
-            }
-            last;
         }
     }
-    
 }
 
 __PACKAGE__->meta->make_immutable;
