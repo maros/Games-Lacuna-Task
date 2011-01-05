75bc9a58c9603daad23c7c09d5ecc45cc908b2e1
Maros Kollar <maros@k-1.com>
2011-01-05 17:41:29 +0100

    Move request related methods to role::client

--- lib/Games/Lacuna/Task/Role/Client.pm
+++ lib/Games/Lacuna/Task/Role/Client.pm
@@ -46,6 +46,91 @@ sub _build_client {
     return $client;
 }
 
+sub request {
+    my ($self,%params) = @_;
+    
+    my $method = delete $params{method};
+    my $type = delete $params{type};
+    my $object = delete $params{object};
+    my $params = delete $params{params} || [];
+    
+    unless (defined $object) {
+        $object = $self
+            ->client
+            ->client
+            ->$type(%params,verbose_rpc => 1);
+    
+        $self->log('debug',"Run external request %s/%s",$type,$method);
+    } else {
+        $self->log('debug',"Run external request %s/%s",ref($object),$method);
+    }
+    my $request = $object->$method(@$params);
+    
+    my $status = $request->{status} || $request;
+    if ($status->{empire}) {
+        $self->write_cache(
+            key     => 'empire',
+            value   => $status->{empire},
+            max_age => 21600,
+        );
+    }
+    if ($status->{body}) {
+        $self->write_cache(
+            key     => 'body/'.$status->{body}{id},
+            value   => $status->{body},
+        );
+    }
+    if ($request->{buildings}) {
+        $self->write_cache(
+            key     => 'body/'.$status->{body}{id}.'/buildings',
+            value   => $request->{buildings},
+            max_age => 600,
+        );
+    }
+    
+    return $request;
+}
+
+
+sub lookup_cache {
+    my ($self,$key) = @_;
+    
+    my $storage = $self->client->storage;
+    my $cache = $storage->lookup($key);
+    return
+        unless defined $cache;
+    if (blessed $cache
+        && $cache->isa('Games::Lacuna::Task::Cache')) {
+        return $cache->value
+    } else {
+        return $cache;
+    }
+}
+
+sub write_cache {
+    my ($self,%params) = @_;
+    
+    my $storage = $self->client->storage;
+    $params{max_age} += time()
+        if $params{max_age};
+   
+    my $cache = Games::Lacuna::Task::Cache->new(
+        %params
+    );
+    
+    $cache->store($storage);
+    
+    return $cache;
+}
+
+sub clear_cache {
+    my ($self,$key) = @_;
+    
+    my $storage = $self->client->storage;
+    $storage->delete($key);
+    
+}
+
 
 no Moose::Role;
 1;
