2787fb21bc571409ffa8ad5d600303ef202321bd
Maros Kollar <maros@k-1.com>
2011-01-05 17:43:03 +0100

    Add ore types to constants

--- lib/Games/Lacuna/Task/Constants.pm
+++ lib/Games/Lacuna/Task/Constants.pm
@@ -3,6 +3,29 @@ package Games::Lacuna::Task::Constants;
 use strict;
 use warnings;
 
-our @RESSOURCES = qw(ore water energy);
+# do not change order
+our @RESSOURCES = qw(water ore energy);
+
+our @ORES = qw(fluorite 
+    anthracite 
+    zircon 
+    chromite 
+    gypsum 
+    sulfur 
+    chalcopyrite 
+    gold 
+    trona 
+    methane 
+    magnetite 
+    halite 
+    rutile 
+    goethite 
+    bauxite
+    kerogen
+    uraninite
+    beryl
+    galena
+    monazite
+);
 
 1;
