78d16870480994aacbd3b9131a6f3deea3c0630b
Maros Kollar <maros@k-1.com>
2011-01-09 11:26:45 +0100

    Do not interrogate/execute prisoners

--- lib/Games/Lacuna/Task/Action/Intelligence.pm
+++ lib/Games/Lacuna/Task/Action/Intelligence.pm
@@ -15,26 +15,11 @@ has 'offensive_assignment' => (
     documentation   => 'Default offensive spy assignment',
 );
 
-has 'prisoners_interogation_assignment' => (
-    isa             => 'Str',
-    is              => 'rw',
-    required        => 1,
-    default         => 'Gather Operative Intelligence',
-    documentation   => 'Default defensive spy assignment if prisoners are being held',
-);
-
 has 'rename_spies' => (
     isa             => 'Bool',
     is              => 'rw',
     default         => 1,
     documentation   => 'Rename spies if they carry the default name',
-);
-
-has 'execute_prisoners' => (
-    isa             => 'Bool',
-    is              => 'rw',
-    default         => 0,
-    documentation   => 'Execute prisoners',
 );
 
 sub description {
@@ -51,7 +36,7 @@ sub process_planet {
     my $intelligence_ministry_object = $self->build_object($intelligence_ministry);
     
     return
-        uness $intelligence_ministry;
+        unless $intelligence_ministry;
     
     my $ministry_data = $self->request(
         object  => $intelligence_ministry_object,
@@ -74,7 +59,7 @@ sub process_planet {
     # Get security ministry
     my ($security_ministry) = $self->find_building($planet_stats->{id},'Security Ministry');
     my @foreign_spies_active;
-    my $has_prisoners = 0;
+    my $foreign_spies_count = 0;
     if ($security_ministry) {
         
         my $security_ministry_object = $self->build_object($security_ministry);
@@ -85,40 +70,16 @@ sub process_planet {
             data    => 'spies',
         );
         
+        $foreign_spies_count = $foreign_spy_data->{spy_count};
+        
         # Check if we have active foreign spies (not idle) that can be discovered via security sweep
         if ($foreign_spy_data->{spy_count} > 0) {
             $self->log('warn',"There are %i foreign spies on %s",$foreign_spy_data->{spy_count},$planet_stats->{name});
-            
             foreach my $spy (@{$foreign_spy_data->{spies}}) {
                 my $next_mission = $self->parse_date($spy->{next_mission});
                 if ($next_mission > $timestamp) {
                     push(@foreign_spies_active,$spy->{level})
                 }
-            }
-        }
-        
-        # Check if we have prisoners
-        my $prisoners_data = $self->paged_request(
-            object  => $security_ministry_object,
-            method  => 'view_prisoners',
-            total   => 'captured_count',
-            data    => 'prisoners',
-        );
-        
-        foreach my $prisoner (@{$prisoners_data->{prisoners}}) {
-            my $happiness_cost = $prisoner->{level} * 10000;
-            
-            # Excecute prisoners if option is set and planet has enough happiness
-            if ($self->execute_prisoners
-                && $planet_stats->{happiness} > $happiness_cost) {
-                $self->paged_request(
-                    object  => $security_ministry_object,
-                    method  => 'execute_prisoner',
-                    params  => [$prisoner->{id}],
-                );
-                $planet_stats->{happiness} -= $happiness_cost;
-            } else {
-                $has_prisoners ++;
             }
         }
     }
@@ -132,13 +93,14 @@ sub process_planet {
     
     # Loop all spies
     my $counter = 1;
+    my $defensive_spy_count = 0;
     my %defensive_spy_assignments;
     foreach my $spy (@{$spy_data->{spies}}) {
         
         # Check if spy has default name
         if ($self->rename_spies
             && lc($spy->{name}) eq 'agent null') {
-            $spy->{name} = sprintf('Agent %02i %s',$counter,$planet_stats->{name});
+            $spy->{name} = sprintf('Agent %02i %-1.1s',$counter,uc($planet_stats->{name}));
             $self->log('notice',"Renaming spy %s on %s",$spy->{name},$planet_stats->{name});
             $self->request(
                 object  => $intelligence_ministry_object,
@@ -151,6 +113,7 @@ sub process_planet {
         if ($spy->{assigned_to}{body_id} == $planet_stats->{id}) {
             $defensive_spy_assignments{$spy->{assignment}} ||= [];
             push(@{$defensive_spy_assignments{$spy->{assignment}}},$spy);
+            $defensive_spy_count ++;
         # Spy is on another planet
         } else {
             next
@@ -185,16 +148,11 @@ sub process_planet {
         
         my $assignment;
         
-        # Interiogate prisoners
-        if ($has_prisoners
-            && ! defined $defensive_spy_assignments{'Gather Operative Intelligence'}
-            && ! defined $defensive_spy_assignments{'Gather Ressource Intelligence'}
-            && ! defined $defensive_spy_assignments{'Gather Empire Intelligence'}) {
-            $assignment = $self->prisoners_interogation_assignment;
         # Run security sweep
-        } elsif (scalar @foreign_spies_active
+        if (scalar @foreign_spies_active
             && ! defined $defensive_spy_assignments{'Security Sweep'}
-            && min(@foreign_spies_active)-1 <= $spy->{level} ) {
+            && min(@foreign_spies_active)-1 <= $spy->{level} 
+            && $defensive_spy_count > $foreign_spies_count) {
             $assignment = 'Security Sweep';
         # Assign to counter espionage
         } elsif ($spy->{assignment} eq 'Idle') {
