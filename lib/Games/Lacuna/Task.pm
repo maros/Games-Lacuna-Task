c77e6b22ba4ebd2317d43ae880c141b8f6b4fed4
Maros Kollar <maros@k-1.com>
2011-01-09 11:25:15 +0100

    Find available tasks via Module::Pluggable

--- lib/Games/Lacuna/Task.pm
+++ lib/Games/Lacuna/Task.pm
@@ -13,6 +13,10 @@ use Moose;
 use Try::Tiny;
 use YAML qw(LoadFile);
 
+use Module::Pluggable 
+    search_path => ['Games::Lacuna::Task::Action'],
+    sub_name => 'all_tasks';
+
 with qw(Games::Lacuna::Task::Role::Client
     Games::Lacuna::Task::Role::Helper
     Games::Lacuna::Task::Role::Logger
@@ -46,40 +50,54 @@ sub run {
     
     my $database_dir = $self->database->dir;
     
+    my @tasks;
+    if (scalar @{$self->task} == 1
+        && lc($self->task->[0]) eq 'all') {
+        @tasks = __PACKAGE__->all_tasks;
+    } else {
+        foreach my $task (@{$self->task}) {
+            my $element = join('',map { ucfirst(lc($_)) } split(/_/,$task));
+            my $class = 'Games::Lacuna::Task::Action::'.$element;
+            push(@tasks,$class)
+                unless $class ~~ \@tasks;
+        }
+    }
+    
     # Loop all tasks
     TASK:
-    foreach my $task (@{$self->task}) {
-        $self->log('notice',("-" x $WIDTH));
-        $self->log('notice',"Running task %s",$task);
+    foreach my $task_class (@tasks) {
+        my $task_name = $task_class;
+        $task_name =~ s/^.+::([^:]+)$/$1/;
+        $task_name = lc($task_name);
         
-        my $element = join('',map { ucfirst(lc($_)) } split(/_/,$task));
-        my $class = 'Games::Lacuna::Task::Action::'.$element;
+        $self->log('notice',("-" x $WIDTH));
+        $self->log('notice',"Running task %s",$task_name);
         
         # Get task config
         my $task_config = {};
-        my $task_config_file = Path::Class::File->new($database_dir,lc($task).'.yml');
+        my $task_config_file = Path::Class::File->new($database_dir,$task_name.'.yml');
         if (-e $task_config_file) {
-            $self->log('debug',"Loading task %s config from",$task,$task_config_file);
+            $self->log('debug',"Loading task %s config from",$task_name,$task_config_file);
             $task_config = LoadFile($task_config_file->stringify);
         }
         
         my $ok = 1;
         try {
-            Class::MOP::load_class($class);
+            Class::MOP::load_class($task_class);
         } catch {
-            $self->log('error',"Could not load task %s: %s",$task,$_);
+            $self->log('error',"Could not load task %s: %s",$task_class,$_);
             $ok = 0;
         };
         if ($ok) {
             try {
-                my $task = $class->new(
+                my $task = $task_class->new(
                     %{$task_config},
                     client  => $client,
                     loglevel=> $self->loglevel,
                 );
                 $task->run;
             } catch {
-                $self->log('error',"An error occured while processing %s: %s",$task,$_);
+                $self->log('error',"An error occured while processing %s: %s",$task_class,$_);
             }
         }
     }
