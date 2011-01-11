6a5000cb61beca1585fa31b6c2a84443f2d2b8b7
Maros Kollar <maros@k-1.com>
2011-01-11 14:40:53 +0100

    Very basic notify role

--- lib/Games/Lacuna/Task/Role/Notify.pm
+++ lib/Games/Lacuna/Task/Role/Notify.pm
@@ -3,16 +3,34 @@ package Games::Lacuna::Task::Role::Notify;
 use 5.010;
 use Moose::Role;
 
+use MIME::Lite;
+
 has 'email' => (
     is              => 'rw',
     isa             => 'Str',
     documentation   => q[Notification e-mail address],
 );
 
+has 'email_send' => (
+    is              => 'rw',
+    isa             => 'ArrayRef',
+    default         => sub { [] },
+    documentation   => q[e-mail send methods],
+);
+
 sub notify {
     my ($self,$subject,$message) = @_;
     
-    warn 'NOT YET IMPLEMENTED!';
+    my $email = MIME::Lite->new(
+        From    => $self->email,
+        To      => $self->email,
+        Subject => $subject,
+        Type    => 'TEXT',
+        Data    => $message,
+    );
+    
+    $email->send( @{ $self->email_send } );
+
 }
 
 no Moose::Role;
