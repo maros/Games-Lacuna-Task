76030e8e99ce556382f25aca46e083e8405f2f84
Maros Kollar <maros@k-1.com>
2011-06-04 22:35:17 +0200

    Vote task: Automatically vote on propositions

---
+++ lib/Games/Lacuna/Task/Action/Vote.pm
@@ -0,0 +1,126 @@
+package Games::Lacuna::Task::Action::Vote;
+
+use 5.010;
+
+use Moose;
+extends qw(Games::Lacuna::Task::Action);
+
+our $BUILDING_COORDINATES_RE = qr/\(-?\d+,-?\d+\)/;
+our $NAME_RE = qr/[[:space:][:alnum:]]+/;
+
+has 'accept_proposition' => (
+    isa             => 'RegexpRef',
+    is              => 'rw',
+    required        => 1,
+    documentation   => 'Propositions matching this regexps should accepted',
+    default         => sub { qr/^( 
+        (Upgrade|Install) \s $NAME_RE \s $BUILDING_COORDINATES_RE
+        |
+        Demolish \s (Dent|Bleeder) \s $BUILDING_COORDINATES_RE
+        |
+        Rename \s $NAME_RE
+        |
+        Repair \s $NAME_RE
+        |
+        Seize \s $NAME_RE
+    )$/xi },
+);
+
+has 'reject_proposition' => (
+    isa             => 'RegexpRef',
+    is              => 'rw',
+    required        => 1,
+    documentation   => 'Propositions matching this regexps should be rejected',
+    default         => sub { qr//xi },
+);
+
+sub description {
+    return q[Parliament vote automation];
+}
+
+sub run {
+    my ($self) = @_;
+    
+    PLANETS:
+    foreach my $body_stats ($self->planets) {
+        next
+            unless $body_stats->{type} eq 'space station';
+        $self->log('info',"Processing space station %s",$body_stats->{name});
+        $self->process_space_station($body_stats);
+    }
+    
+    my $inbox_object = $self->build_object('Inbox');
+    
+    # Get inbox for parliament
+    my $inbox_data = $self->request(
+        object  => $inbox_object,
+        method  => 'view_inbox',
+        params  => [{ tags => ['Parliament'],page_number => 1 }],
+    );
+    
+    my @trash_messages;
+    
+    MESSAGES:
+    foreach my $message (@{$inbox_data->{messages}}) {
+        if ($message->{subject} =~ m/^(Pass|Reject):\s+/
+            || $message->{subject} =~ $self->accept_proposition
+            || $message->{subject} =~ $self->reject_proposition) {
+            push(@trash_messages,$message->{id});
+        }
+    }
+    
+    # Archive
+    if (scalar @trash_messages) {
+        $self->log('notice',"Trashing %i messages",scalar @trash_messages);
+        
+        $self->request(
+            object  => $inbox_object,
+            method  => 'trash_messages',
+            params  => [\@trash_messages],
+        );
+    }
+}
+
+sub process_space_station {
+    my ($self,$station_stats) = @_;
+    
+    # Get parliament ministry
+    my ($parliament) = $self->find_building($station_stats->{id},'Parliament');
+    return
+        unless $parliament;
+    my $parliament_object = $self->build_object($parliament);
+    
+    my $proposition_data = $self->request(
+        object  => $parliament_object,
+        method  => 'view_propositions',
+    );
+    
+    PROPOSITION:
+    foreach my $proposition (@{$proposition_data->{propositions}}) {
+        next PROPOSITION
+            if defined $proposition->{my_vote};
+        
+        my $vote;
+        
+        if ($proposition->{name} =~ $self->accept_proposition) {
+            $vote = 1;
+        } elsif ($proposition->{name} =~ $self->reject_proposition) {
+            $vote = 0;
+        } else {
+            next PROPOSITION;
+        }
+        
+        $self->log('notice','Voting %s on proposition %s',($vote ? 'Yes':'No'),$proposition->{name});
+        
+        $self->request(
+            object  => $parliament_object,
+            method  => 'cast_vote',
+            params  => [$proposition->{id},$vote],
+        );
+    
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
