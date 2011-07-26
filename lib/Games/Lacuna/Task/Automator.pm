b5107666e88c6e63ed769a877e8d834507c346c5
Maros Kollar <maros@k-1.com>
2011-07-26 22:04:24 +0200

    Refactor run code

--- lib/Games/Lacuna/Task/Automator.pm
+++ lib/Games/Lacuna/Task/Automator.pm
@@ -5,19 +5,9 @@ use 5.010;
 use Moose;
 with qw(Games::Lacuna::Task::Role::Client
     Games::Lacuna::Task::Role::Helper
-    Games::Lacuna::Task::Role::Logger);
-
-sub run {
-    my ($self) = @_;
-    
-    PLANETS:
-    foreach my $planet_stats ($self->planets) {
-        next
-            unless $planet_stats->{type} eq 'habitable planet';
-        $self->log('info',"Processing planet %s",$planet_stats->{name});
-        $self->process_planet($planet_stats);
-    }
-}
+    Games::Lacuna::Task::Role::Logger
+    Games::Lacuna::Task::Role::PlanetRun
+    );
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
---
+++ lib/Games/Lacuna/Task/Role/PlanetRun.pm
@@ -0,0 +1,19 @@
+package Games::Lacuna::Task::Role::PlanetRun;
+
+use 5.010;
+use Moose::Role;
+
+sub run {
+    my ($self) = @_;
+    
+    PLANETS:
+    foreach my $planet_stats ($self->planets) {
+        next
+            unless $planet_stats->{type} eq 'habitable planet';
+        $self->log('info',"Processing planet %s",$planet_stats->{name});
+        $self->process_planet($planet_stats);
+    }
+}
+
+no Moose::Role;
+1;
