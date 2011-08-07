394e0d78d8c919d5b2570c5c1823845cca41b0b8
Maros Kollar <maros@k-1.com>
2011-08-07 08:17:31 +0200

    Exclude planets option

--- lib/Games/Lacuna/Task/Role/PlanetRun.pm
+++ lib/Games/Lacuna/Task/Role/PlanetRun.pm
@@ -3,11 +3,33 @@ package Games::Lacuna::Task::Role::PlanetRun;
 use 5.010;
 use Moose::Role;
 
+has 'exclude_planet' => (
+    is              => 'rw',
+    isa             => 'ArrayRef[Str]',
+    documentation   => 'Do not process given planets',
+    traits          => ['Array'],
+    default         => sub { [] },
+    handles         => {
+        'has_exclude_planet' => 'count',
+    }
+);
+
 sub run {
     my ($self) = @_;
     
+    my @exclude_planets;
+    if ($self->has_exclude_planet) {
+        foreach my $planet (@{$self->exclude_planet}) {
+            my $planet_id = $self->my_body_id($planet);
+            push(@exclude_planets,$planet_id)
+                if $planet_id;
+        }
+    }
+    
     PLANETS:
     foreach my $planet_stats ($self->my_planets) {
+        next PLANETS
+            if $planet_stats->{id} ~~ \@exclude_planets;
         $self->log('info',"Processing planet %s",$planet_stats->{name});
         $self->process_planet($planet_stats);
     }
