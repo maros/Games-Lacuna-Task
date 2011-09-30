428e0da98daa52fb6b109198e6b8b299252a1a28
Maros Kollar <maros@k-1.com>
2011-09-30 09:45:35 +0200

    Check if we can handle mission reward

--- lib/Games/Lacuna/Task/Action/Mission.pm
+++ lib/Games/Lacuna/Task/Action/Mission.pm
@@ -42,35 +42,26 @@ sub process_planet {
     
     MISSIONS:
     foreach my $mission (@{$mission_data->{missions}}) {
-        next
-            unless lc($mission->{name}) ~~ lc($self->missions);
+        my $objectives = $self->parse_mission($mission->{objectives});
+        next MISSIONS
+            unless $mission->{name} ~~ $self->missions;
         
+        next MISSIONS;
+        # Check if we have the required resources
         my @used_plans;
-        foreach my $objective (@{$mission->{objectives}}) {
-            given ($objective) {
-                when (m/^
-                    (?<name>[^(]+)
-                    \s
-                    \(
-                        >=
-                        \s
-                        (?<level>\d+)
-                        (\+(?<extra_build_level>\d+))?
-                    \)
-                    \s
-                    plan$/x) {
+        foreach my $objective (@{$objectives}) {
+            given ($objective->{type}) {
+                when ('plan') {
                     $plans ||= $self->plans_stored($planet_stats->{id});
-                    
                     my $found_plan = 0;
                     PLANS:
                     foreach my $plan (@$plans) {
-                        
                         next PLANS
-                            unless $plan->{name} eq $+{name};
+                            unless $plan->{name} eq $objective->{plan};
                         next PLANS
-                            if $plan->{level} != $+{level};
+                            if $plan->{level} != $objective->{level};
                         next PLANS
-                            if $plan->{extra_build_level} != ($+{extra_build_level} // 0);
+                            if $plan->{extra_build_level} != $objective->{extra_build_level};
                         next PLANS
                             if grep { $plan == $_ } @used_plans;
                         push (@used_plans,$plan);
@@ -79,49 +70,39 @@ sub process_planet {
                     next MISSIONS
                         unless $found_plan;
                 }
-                when (m/^(?<quantity>[,0-9]+)\s(?<resource>.+)$/) {
-                    my $quantity = $+{quantity};
-                    my $resource = $+{resource};
-                    $quantity =~ s/\D//g;
-                    $quantity += 0;
+                when ('resource') {
                     next MISSIONS
-                        if $quantity > $self->check_stored($planet_stats,$resource);
+                        if $objective->{quantity} > $self->check_stored($planet_stats,$objective->{resource});
                 }
-                when (m/^(?<resource>.+)\sglyph$/) {
-                    # no check
-                }
-                default {
-                    # not implemented
-                    next MISSIONS;
-                }
-#                when (m/^
-#                    (?<ship>[^(]+)
-#                    \s
-#                    \(
-#                        speed \s >= (?<speed>\d+),
-#                        \s
-#                        stealth \s >= (?<stealth>\d+),
-#                        \s
-#                        hold \s size \s >= (?<hold>\d+),
-#                        \s
-#                        combat \s >= (?<combat>\d+),
-#                    \)
-#                    $/x) {
-#                    # TODO check ship
-#                }
             }
         }
         
-        # TODO check if we can store rewards
+        my $rewards = $self->parse_mission($mission->{rewards});
+        
+        my %rewards_resources = map { $_ => 0 } @Games::Lacuna::Task::Constants::RESOURCES;
+        # Check if we have can handle the reward
+        foreach my $reward (@{$rewards}) {
+            if ($reward->{type} eq 'resource') {
+                my $resource_type = $self->resource_type($reward->{resource});
+                $rewards_resources{$resource_type} += $reward->{quantity};
+            }
+        }
+        
+        foreach my $resource_type (@Games::Lacuna::Task::Constants::RESOURCES) {
+            my $capacity = $planet_stats->{$resource_type.'_capacity'} - $planet_stats->{$resource_type.'_stored'} - $rewards_resources{$resource_type};
+            next MISSION
+                if $capacity < -100000;
+        }
         
         $self->log('notice',"Completing mission %s on %s",$mission->{name},$planet_stats->{name});
         
         try {
-            $self->request(
+            my $response = $self->request(
                 object  => $missioncommand_object,
                 method  => 'complete_mission',
                 params  => [$mission->{id}],
             );
+            $planet_stats = $response->{status}{body};
         } catch {
             my $error = $_;
             if (blessed($error)
@@ -136,6 +117,108 @@ sub process_planet {
             }
         };
     }
+}
+
+sub parse_mission {
+    my ($self,$list) = @_;
+    
+    my @parsed;
+    
+    foreach my $element (@{$list}) {
+        given ($element) {
+            when (m/^
+                (?<plan>[^(]+)
+                \s
+                \(
+                    >=
+                    \s
+                    (?<level>\d+)
+                    (\+(?<extra_build_level>\d+))?
+                \)
+                \s
+                plan$/x) {
+                
+                push(@parsed,{
+                    type                => 'plan',
+                    level               => $+{level},
+                    plan                => $+{plan},
+                    extra_build_level   => ($+{extra_build_level} // 0)
+                });
+            }
+            when (m/^(?<quantity>[,0-9]+)\s(?<resource>.+)$/) {
+                my $quantity = $+{quantity};
+                my $resource = $+{resource};
+                $quantity =~ s/\D//g;
+                $quantity += 0;
+                push(@parsed,{
+                    type                => 'resource',
+                    resource            => $resource,
+                    quantity            => $quantity,
+                });
+            }
+            when (m/^(?<glyph>.+)\sglyph$/) {
+                push(@parsed,{
+                    type                => 'glyph',
+                    glyph               => $+{glyph},
+                });
+            }
+            when (m/^
+                (?<ship>[^(]+)
+                \s
+                \(
+                    speed \s >= \s (?<speed>[0-9,]+),
+                    \s
+                    stealth \s >= \s (?<stealth>[0-9,]+),
+                    \s
+                    hold \s size \s >= \s (?<hold>[0-9,]+),
+                    \s
+                    combat \s >= \s (?<combat>[0-9,]+)
+                \)
+                $/x) {
+                my $ship = $+{ship};
+                my $speed = $+{speed};
+                my $hold = $+{hold};
+                my $stealth = $+{stealth};
+                my $combat = $+{combat};
+                $speed =~ s/\D//g;
+                $hold =~ s/\D//g;
+                $stealth =~ s/\D//g;
+                $combat =~ s/\D//g;
+                push(@parsed,{
+                    type                => 'ship',
+                    ship                => $ship,
+                    stealth             => $stealth,
+                    hold                => $hold,
+                    combat              => $combat,
+                    speed               => $speed,
+                });
+            }
+            when (m/^Send 
+                \s 
+                (?<ship>.+?) 
+                \s to \s 
+                (?<planet>.+?) 
+                \s 
+                \(
+                (?<x>-?\d+)
+                ,
+                (?<y>-?\d+)
+                \)/x) {
+                push(@parsed,{
+                    type                => 'send',
+                    ship                => $+{ship},
+                    planet              => $+{planet},
+                    x                   => $+{x},
+                    y                   => $+{y},
+                });
+            }
+            default {
+                warn $_;
+            }
+        }
+    }
+    
+    return \@parsed;
 }
 
 __PACKAGE__->meta->make_immutable;
