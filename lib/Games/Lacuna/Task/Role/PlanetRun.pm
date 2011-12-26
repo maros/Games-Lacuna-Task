4f10f0160e86b4ae534bd1fb1877d07b9e0154f7
Maros Kollar <maros@k-1.com>
2011-12-26 11:33:26 +0100

    Add 'only_planet' option to planet run role

--- lib/Games/Lacuna/Task/Role/PlanetRun.pm
+++ lib/Games/Lacuna/Task/Role/PlanetRun.pm
@@ -14,22 +14,48 @@ has 'exclude_planet' => (
     }
 );
 
+has 'only_planet' => (
+    is              => 'rw',
+    isa             => 'ArrayRef[Str]',
+    documentation   => 'Only process given planets',
+    traits          => ['Array'],
+    default         => sub { [] },
+    handles         => {
+        'has_only_planet' => 'count',
+    }
+);
+
 sub run {
     my ($self) = @_;
     
-    my @exclude_planets;
-    if ($self->has_exclude_planet) {
+    my @planets;
+    
+    # Only selected planets
+    if ($self->has_only_planet) {
+        foreach my $only_planet (@{$self->only_planet}) {
+            my $planet = $self->my_body_status($only_planet);
+            push(@planets,$planet)
+                if $planet;
+        }
+    # All but selected planets
+    } elsif ($self->has_exclude_planet) {
+        my @exclude_planets;
         foreach my $planet (@{$self->exclude_planet}) {
             my $planet_id = $self->my_body_id($planet);
             push(@exclude_planets,$planet_id)
                 if $planet_id;
         }
+        foreach my $planet_stats ($self->my_planets) {
+            push(@planets,$planet_stats)
+                unless $planet_stats->{id} ~~ \@exclude_planets;
+        }
+    # All planets
+    } else {
+        @planets = $self->my_planets;
     }
     
     PLANETS:
-    foreach my $planet_stats ($self->my_planets) {
-        next PLANETS
-            if $planet_stats->{id} ~~ \@exclude_planets;
+    foreach my $planet_stats (@planets) {
         $self->log('info',"Processing planet %s",$planet_stats->{name});
         $self->process_planet($planet_stats);
     }
