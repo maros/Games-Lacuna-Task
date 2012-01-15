7d1987c9a9d5aee20d50fe47a1a606983e9954c5
Maros Kollar <maros@k-1.com>
2012-01-15 20:27:51 +0100

    Basic locking

--- lib/Games/Lacuna/Task/Base.pm
+++ lib/Games/Lacuna/Task/Base.pm
@@ -17,9 +17,58 @@ use Module::Pluggable
     search_path => ['Games::Lacuna::Task::Action'],
     sub_name => '_all_actions';
 
+
+has 'lockfile' => (
+    is              => 'rw',
+    isa             => 'Path::Class::File',
+    traits          => ['NoGetopt'],
+    lazy_build      => 1,
+);
+
+sub _build_lockfile {
+    my ($self) = @_;
+    
+    return $self->configdir->file('lacuna.pid');
+}
+
+sub BUILD {
+    my ($self) = @_;
+    
+    my $lockcounter = 0;
+    my $lockfile = $self->lockfile;
+    
+    warn "RUN BUILD...";
+    # Check for lockfile
+    while (-e $lockfile) {
+        my ($pid) = $lockfile->slurp(chomp => 1);
+        
+        if ($lockcounter > 10) {
+            $self->abort('Could not aquire lock');
+        } else {
+            $self->log('warn','Another process is currently running. Waiting until it has finished');
+        }
+        $lockcounter++;
+        sleep 60;
+    }
+    
+    # Write lock file
+    my $lockfh = $lockfile->openw();
+    print $lockfh $$;
+    $lockfh->close;
+}
+
+sub DEMOLISH {
+    my ($self) = @_;
+    
+    $self->lockfile->remove
+        if -e $self->lockfile;
+}
+
 sub all_actions {
     _all_actions()
 }
+
+
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
