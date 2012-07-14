7bddbc3ce4862b9a9c7f6fcdd6a7028972776edc
Maros Kollar <maros@k-1.com>
2012-07-14 09:27:15 +0200

    Build queue report and halls of vrbansk report

---
+++ lib/Games/Lacuna/Task/Report/BuildQueue.pm
@@ -0,0 +1,55 @@
+package Games::Lacuna::Task::Report::BuildQueue;
+
+use 5.010;
+our $VERSION = $Games::Lacuna::Task::VERSION;
+
+use Moose::Role;
+
+use Games::Lacuna::Task::Utils qw(parse_date format_date format_duration);
+
+sub report_build_queue {
+    my ($self) = @_;
+    
+    my $table = Games::Lacuna::Task::Table->new(
+        headline=> 'Build Queue Report',
+        columns => ['Planet','Size','Finished at','Finished in'],
+    );
+    
+    foreach my $planet_id ($self->my_planets) {
+        $self->_report_build_queue_body($planet_id,$table);
+    }
+    
+    return $table;
+}
+
+sub _report_build_queue_body {
+    my ($self,$planet_id,$table) = @_;
+    
+    my $planet_stats = $self->my_body_status($planet_id);
+    my @buildings = $self->buildings_body($planet_stats);
+
+    my $timestamp = time();
+    my $building_count = 0;
+    my $max_date_end = 0;
+    
+    foreach my $building_data (@buildings) {
+        next
+            unless (defined $building_data->{pending_build});
+        my $date_end = parse_date($building_data->{pending_build}{end});
+        next
+            if $timestamp > $date_end;
+        $building_count ++;
+        $max_date_end = $date_end
+            if $date_end > $max_date_end;
+    }
+    
+    $table->add_row({
+        planet          => $planet_stats->{name},
+        size            => $building_count,
+        finished_at     => ($max_date_end ? format_date($max_date_end) : 'now'),
+        finished_in     => format_duration($max_date_end),
+    });
+}
+
+no Moose::Role;
+1;
---
+++ lib/Games/Lacuna/Task/Report/Vrbansk.pm
@@ -0,0 +1,66 @@
+package Games::Lacuna::Task::Report::Vrbansk;
+
+use 5.010;
+our $VERSION = $Games::Lacuna::Task::VERSION;
+
+use Moose::Role;
+with qw(Games::Lacuna::Task::Role::Storage);
+
+use Games::Lacuna::Client::Types qw(ore_types);
+
+sub report_vrbansk {
+    my ($self) = @_;
+    
+    my $table = Games::Lacuna::Task::Table->new(
+        headline=> 'Halls of Vrbansk Report',
+        columns => ['Planet','Plans','Built','Total'],
+    );
+    
+    my $total = 0;
+    foreach my $planet_id ($self->my_planets) {
+       $total += $self->_report_vrbansk_body($planet_id,$table);
+    }
+    
+    $table->add_row({
+        planet          => 'Total',
+        total           => $total,
+    });
+    
+    return $table;
+}
+
+sub _report_vrbansk_body {
+    my ($self,$planet_id,$table) = @_;
+    
+    my $planet_stats = $self->my_body_status($planet_id);
+    my $plans_stored = $self->plans_stored($planet_id);
+    my @buildings = $self->buildings_body($planet_stats);
+    
+    my ($plans,$built,$total) = (0,0,0);
+    foreach my $plan (@{$plans_stored}) {
+        next
+            unless $plan->{name} eq 'Halls of Vrbansk';
+        $plans = $plan->{quantity};
+        last;
+    }
+    
+    foreach my $building (@buildings) {
+        next
+            unless $building->{name} eq 'Halls of Vrbansk';
+        $built++;
+    }
+    
+    $total = $built+$plans;
+    
+    $table->add_row({
+        planet          => $planet_stats->{name},
+        plans           => $plans,
+        built           => $built,
+        total           => $total,
+    });
+    
+    return $total;
+}
+
+no Moose::Role;
+1;
