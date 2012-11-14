9150b9f9efabff47902eaad19ee49a559be9de8c
Maros Kollar <maros@k-1.com>
2012-11-14 17:54:54 +0100

    Fix probes destroyed action

--- lib/Games/Lacuna/Task/Action/ShipDestroyed.pm
+++ lib/Games/Lacuna/Task/Action/ShipDestroyed.pm
@@ -15,80 +15,86 @@ sub run {
     my ($self) = @_;
     
     my $inbox_object = $self->build_object('Inbox');
-    
-    my $inbox_data = $self->request(
-        object  => $inbox_object,
-        method  => 'view_inbox',
-        params  => [{ tags => ['Spies','Probe'],page_number => 1 }],
-    );
-    
-    die $inbox_data;
+    my $page_number = 1;
     
     my @star_checked;
     my @archive_messages;
-    
-    foreach my $message (@{$inbox_data->{messages}}) {
-        next
-            unless $message->{from_id} == $message->{to_id};
-        
-        my $type;
-        
-        given ($message->{subject}) {
-            when ('Probe Destroyed') {
-                $type = 'probe';
-            }
-            when ('Lost Contact With Probe') {
-                $type = 'probe';
-            }
-            when ('Ship Shot Down') {
-                $type = 'ship';
-            }
-            default {
-                next;   
-            }
-        }
-        
-        
-        # Get message
-        my $message_data = $self->request(
+
+    while (1) {
+        # Get inbox for parliament
+        my $inbox_data = $self->request(
             object  => $inbox_object,
-            method  => 'read_message',
-            params  => [$message->{id}],
+            method  => 'view_inbox',
+            params  => [{page_number => $page_number }],
         );
-            
-        # Parse star id,x,y
-        next
-            unless $message_data->{message}{body} =~ m/\{Starmap\s(?<x>-*\d+)\s(?<y>-*\d+)\s(?<body_name>[^}]+)\}/;
-            
-        my $body_name = $+{body_name};
-        my $body_data = $self->get_body_by_xy($+{x},$+{y});
         
-        my $star_data;
-        if (defined $body_data) {
-            $star_data = $self->get_star($body_data->{star_id});
-        } else {
-            $star_data = $self->get_star_by_xy($+{x},$+{y});
-        }
-            
-        next
-            unless $star_data;
-            
-        unless ($star_data->{id} ~~ \@star_checked) {
-            $self->_get_star_api($star_data->{id},$star_data->{x},$star_data->{y});
-            push (@star_checked,$star_data->{id});
-        }
-        
-        if ($type eq 'probe') {
+        foreach my $message (@{$inbox_data->{messages}}) {
             next
-                unless $message_data->{message}{body} =~ m/{Empire\s(?<empire_id>\d+)\s(?<empire_name>[^}]+)}/;
+                unless $message->{from_id} == $message->{to_id};
+            my $type;
+            
+            given ($message->{subject}) {
+                when ('Probe Destroyed') {
+                    $type = 'probe';
+                }
+                when ('Lost Contact With Probe') {
+                    $type = 'probe';
+                }
+                when ('Ship Shot Down') {
+                    $type = 'ship';
+                }
+                default {
+                    next;   
+                }
+            }
+            
+            push(@archive_messages,$message->{id});
+
+            # Get message
+            my $message_data = $self->request(
+                object  => $inbox_object,
+                method  => 'read_message',
+                params  => [$message->{id}],
+            );
                 
-            $self->log('warn','A probe in the %s system was destroyed by %s',$body_name,$+{empire_name});
-        } elsif ($type eq 'ship') {
-            $self->log('warn','Our ship on way to %s was shot down',$body_name);
+            # Parse star id,x,y
+            next
+                unless $message_data->{message}{body} =~ m/\{Starmap\s(?<x>-*\d+)\s(?<y>-*\d+)\s(?<body_name>[^}]+)\}/;
+                
+            my $body_name = $+{body_name};
+            my $body_data = $self->get_body_by_xy($+{x},$+{y});
+            
+            my $star_data;
+            if (defined $body_data) {
+                $star_data = $self->get_star($body_data->{star_id});
+            } else {
+                $star_data = $self->get_star_by_xy($+{x},$+{y});
+            }
+                
+            next
+                unless $star_data;
+                
+            unless ($star_data->{id} ~~ \@star_checked) {
+                $self->_get_star_api($star_data->{id},$star_data->{x},$star_data->{y});
+                push (@star_checked,$star_data->{id});
+            }
+            
+            if ($type eq 'probe') {
+                next
+                    unless $message_data->{message}{body} =~ m/{Empire\s(?<empire_id>\d+)\s(?<empire_name>[^}]+)}/;
+                    
+                $self->log('warn','A probe in the %s system was destroyed by %s',$body_name,$+{empire_name});
+            } elsif ($type eq 'ship') {
+                $self->log('warn','Our ship on way to %s was shot down',$body_name);
+            }
         }
+
+        last
+            if scalar(@{$inbox_data->{messages}}) < 25;
         
-        push(@archive_messages,$message->{id});
+        $page_number++;
     }
+    
     
     # Archive
     if (scalar @archive_messages) {
