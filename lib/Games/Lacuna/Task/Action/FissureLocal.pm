c742d70b83fcfbabbe6818aa4c9cd21916b00a67
Maros Kollar <maros@k-1.com>
2013-05-13 22:29:44 +0200

    New fissure sealing action

---
+++ lib/Games/Lacuna/Task/Action/FissureLocal.pm
@@ -0,0 +1,81 @@
+package Games::Lacuna::Task::Action::FissureLocal;
+
+use 5.010;
+our $VERSION = $Games::Lacuna::Task::VERSION;
+
+use Moose;
+extends qw(Games::Lacuna::Task::Action);
+with qw(Games::Lacuna::Task::Role::PlanetRun);
+
+use List::Util qw(max sum);
+
+sub description {
+    return q[Downgrade fissures on own planets];
+}
+
+sub process_planet {
+    my ($self,$planet_stats) = @_;
+    
+    # Get archaeology ministry
+    my @fissures = $self->find_building($planet_stats->{id},'Fissure');
+    
+    return
+        unless defined scalar @fissures;
+    
+    foreach my $fissure (@fissures) {
+        $self->log(
+            'warn',
+            'Found level %i fissure at %s',
+            $fissure->{level},
+            $planet_stats->{name}
+        );
+        
+        my $fissure_object = $self->build_object($fissure);
+        
+        my $fissure_data = $self->request(
+            object  => $fissure_object,
+            method  => 'view',
+        );
+        
+        while (1) {
+            my $building_data = $fissure_data->{building};
+            
+            if ($building_data->{level} == 1) {
+                $self->log('notice','Demolish fissure on %s',$planet_stats->{name});
+                
+                $self->request(
+                    object  => $fissure_object,
+                    method  => 'demolish',
+                );
+                
+                last;
+            } elsif ($building_data->{downgrade}{can}) {
+                $self->log('notice','Downgrade fissure on %s',$planet_stats->{name});
+                
+                $fissure_data = $self->request(
+                    object  => $fissure_object,
+                    method  => 'downgrade',
+                );
+            } else {
+                last;
+            }
+        }
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Games::Lacuna::Task::Action::Fissure - Downgrade fissures
+
+=head1 DESCRIPTION
+
+This task will automate the downgrade and demoloition of fissures.
+
+=cut
+
---
+++ lib/Games/Lacuna/Task/Action/FissureRemote.pm
@@ -0,0 +1,83 @@
+package Games::Lacuna::Task::Action::FissureRemote;
+
+use 5.010;
+our $VERSION = $Games::Lacuna::Task::VERSION;
+
+use Moose;
+extends qw(Games::Lacuna::Task::Action);
+with 'Games::Lacuna::Task::Role::Stars',
+    'Games::Lacuna::Task::Role::Inbox',
+    'Games::Lacuna::Task::Role::Ships',
+    'Games::Lacuna::Task::Role::CommonAttributes' => { attributes => ['home_planet'] };
+
+use List::Util qw(max sum);
+
+sub description {
+    return q[Downgrade fissures on remote planets];
+}
+
+sub run {
+    my ($self) = @_;
+    
+    my $planet_home = $self->home_planet_data();
+    
+    # Get spaceport
+    my ($spaceport) = $self->find_building($planet_home->{id},'Spaceport');
+    return $self->log('error','Could not find spaceport')
+        unless (defined $spaceport);
+    my $spaceport_object = $self->build_object($spaceport);
+    
+    
+    $self->inbox_callback(sub {
+            my ($message) = @_;
+            
+            return 0
+                unless ($message->{subject} =~ m/\b[fF]issure\b/);
+            
+            my $message_data = $self->inbox_read($message->{id});
+            my $body_data = $self->get_body_by_xy($message_data->{starmap}{x},$message_data->{starmap}{y});
+            
+            return 0
+                unless defined $body_data;
+                
+            # Get available fissure sealer ships
+            my @avaliable_fissure_sealer = $self->get_ships(
+                planet          => $planet_home,
+                quantity        => 3,
+                type            => 'fissure_sealer',
+            );
+            
+            return 0
+                unless scalar @avaliable_fissure_sealer == 3;
+            
+            my $response = $self->request(
+                object      => $spaceport_object,
+                method      => 'send_fleet',
+                params      => [ \@avaliable_fissure_sealer,{ 'body_id' => $body_data->{id} }],
+            );
+            
+            $self->log('notice','Sent %i fissure sealers to %s',3,$response->{fleet}[0]{ship}{to}{name});
+            
+            return 1;
+        },
+        tags    => 'Alert',
+        archive => 1,
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Games::Lacuna::Task::Action::Fissure - Downgrade fissures
+
+=head1 DESCRIPTION
+
+This task will automate the downgrade and demoloition of fissures.
+
+=cut
+
